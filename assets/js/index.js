e3f284a4efeba42c442340445e42e22388079f81
Fakorede Damilola <dammy.fakorede@gmail.com>
2021-10-08 20:03:06 +0100

    added latest UI design

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,6 +6,15 @@ let closeNav = document.querySelector(".close-nav")
 let menu = document.querySelector(".menu")
 let multiNavbar = document.querySelector(".multi_navbar")
 let multiNavbarUl = document.querySelector(".multi_navbar_ul")
+
+// DEADLINE
+let deadline = "2021-11-21 10:00";
+let daysVal = document.querySelector(".daysVal");
+let hoursVal = document.querySelector(".hoursVal");
+let minutesVal = document.querySelector(".minutesVal");
+let secondsVal = document.querySelector(".secondsVal");
+let deadlineDate = new Date(deadline);
+
 // define eventlisteners
 let navbar = false
 menuToggle.addEventListener("click", function () {
@@ -21,6 +30,31 @@ menuToggle.addEventListener("click", function () {
     }
 
 })
+
+const createValue = time=> {
+    let days = Math.floor(time / (3600 * 24));
+    let hours = Math.floor((time % (3600 * 24)) / 3600);
+    let minutes = Math.floor((time % 3600) / 60);
+    let seconds = Math.floor(time % 60);
+    let values = { days, hours, minutes, seconds };
+    return values;
+  }
+  const setTimeUI = time => {
+    let x = setInterval(function () {
+      let now = new Date().getTime();
+      let later = time.getTime();
+      let timeDiff = (later - now) / 1000;
+      let values = createValue(timeDiff);
+      daysVal.innerHTML = values.days;
+      hoursVal.innerHTML = values.hours > 9 ? values.hours : `0${values.hours}`;
+      minutesVal.innerHTML =
+        values.minutes > 9 ? values.minutes : `0${values.minutes}`;
+      secondsVal.innerHTML =
+        values.seconds > 9 ? values.seconds : `0${values.seconds}`;
+    }, 1000);
+  }
+  
+document.addEventListener("DOMContentLoaded",()=> setTimeUI(deadlineDate))
 
 multiNavbar.addEventListener("click", function () {
     multiNavbarUl.classList.toggle("active")
@@ -42,7 +76,7 @@ fixModalCancel.addEventListener("click",(e)=>{
 setTimeout(()=> {
     fixModal.style.display="block"
     page.classList.toggle("blur")
-},10000)
+},15000)
 
 // QUESTIONS
 let show = false
@@ -100,6 +134,7 @@ let pText = document.querySelectorAll(".pText")
 function animation() {
     let counter = 0
     animateOne[counter].classList.add("startCircleAnimation")
+
     setTimeout(() => animateOne[counter].classList.add("startAnimateOne"), 300)
     let animateInterval = setInterval(() => {
         if (counter < animateOne.length - 1) {
